7bf4a13fef69b09ca235483cd8cef1c18403b910
show1597 <show22668844@gmail.com>
2018-03-22 17:25:55 +0800

    新增刪除修改

--- src/taipower_Jsoup/testconsole.java
+++ src/taipower_Jsoup/testconsole.java
@@ -1,5 +1,6 @@
 package taipower_Jsoup;
 import java.net.URL;
+import java.sql.*;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -29,9 +30,9 @@ import org.jsoup.select.Elements;
 public class testconsole {
 
 	
-	
+	//SSL硈絬
 	final static String[] URL= {"https://www.taipower.com.tw/tc/rssNews.ashx","https://www.taipower.com.tw/tc/rssEvents.ashx","https://www.taipower.com.tw/tc/rssMeasures.ashx"};
-	 
+	
     public static void enableSSLSocket() throws KeyManagementException, NoSuchAlgorithmException {
         HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
             public boolean verify(String hostname, SSLSession session) {
@@ -54,16 +55,30 @@ public class testconsole {
         HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
     }
 	
+    //磅︽
 	public static void main(String[] JsoupURL) throws Exception {
 		// TODO Auto-generated method stub
 		enableSSLSocket();
-		Parsing_banner();
-		for(int i=0;i< JsoupURL.length;i++)
+		//Parsing_banner();
+		
+		
+		/*for(int i=0;i< URL.length;i++)
 		{
-			Parsing_news(JsoupURL[i]);
-		}
+			Parsing_Insert(URL[i],i+1);
+		}*/
+		
+		
+		/*for(int i=0;i< URL.length;i++)
+		{
+			Parsing_Update(URL[i],i+1);
+		}*/
+		String ID="117";
+		//Parsing_Delete(ID);
+		Parsing_Select(ID);
+		
 	}
-	//parding絤策-瓜
+	
+	//parsing絤策-瓜
 	public static void Parsing_banner() throws Exception{
 		Document doc = Jsoup.connect("https://www.taipower.com.tw/tc/index.aspx").get();
 		System.out.print(doc.title());
@@ -72,43 +87,126 @@ public class testconsole {
 			System.out.print(headline.absUrl("src")+" "+headline.absUrl("href")+"\n");
 		}
 	}
-	//Parsing絤策-穝籇
-	public static void Parsing_news(String URL) throws Exception{
+	//Parsing絤策-Insert
+	public static void Parsing_Insert(String URL,int count) throws Exception{
 		Document doc = Jsoup.connect(URL).get();
-		//Elements newsHeadlines = doc.select("title,pubDate,description,tcNewsID");
 		Elements title = doc.select("title");
 		Elements pubDate = doc.select("pubDate");
 		Elements description = doc.select("description");
 		Elements tcNewsID = doc.select("tcNewsID");
 		Locale local = new Locale("TRADITIONAL_CHINESE");
 		DateFormat df = new SimpleDateFormat("E, dd MMM yyy HH:mm:ss z",local);
-		DateFormat nowdf = new SimpleDateFormat("dd/MM/yyyy",local);
-		Date sdate = new Date();
-		Calendar cal = Calendar.getInstance();
-		Calendar nowcal = Calendar.getInstance();
-		nowcal.setTime(sdate);
-		String matdate = df.format(nowcal.getTime());
-		int helfmonth = nowcal.get(Calendar.MONTH);
-		int helfyear = nowcal.get(Calendar.YEAR);
-		if(helfmonth<=6) {
-			helfyear = helfyear-1;
-			helfmonth=12 + helfmonth - 6;
-		}
-		else {
-			helfmonth=helfmonth-6;
-		}
-		for(int i=0 ; i<pubDate.size();i++) {
-			Date date = df.parse(pubDate.get(i).text());
-			cal.setTime(date);
-			if(((cal.get(Calendar.YEAR) == helfyear)&&cal.get(Calendar.MONTH) >= helfmonth)|| cal.get(Calendar.YEAR)>helfyear) {
-				cal.add(Calendar.YEAR, -1911);
-				String formatdate = df.format(cal.getTime());
-				System.out.println(title.get(i+1).text());
-				System.out.println(formatdate);
-				System.out.println(description.get(i+1).text());
-				System.out.println(tcNewsID.get(i).text());
-			}
-		}
-	}
+		DateFormat insertdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		
+		try { 
+            String url = "jdbc:sqlserver://114.34.8.237;databaseName=Taipower";
+            Connection conn = DriverManager.getConnection(url,"show0308","show0308"); 
+            Statement st = conn.createStatement(); 
+            for(int i=0 ; i<pubDate.size();i++) {
+    			Date date = df.parse(pubDate.get(i).text());
+    			
+    			
+    				String formatdate = insertdf.format(date);
+    				
+    				String sql = "INSERT INTO dbo.NEWS (Title"
+    						+ ",Pubtime"
+    						+ ",Description"
+    						+ ",tcNewsID"
+    						+ ",Type)" + 
+    		                "VALUES ('"+title.get(i+1).text()
+    		                +"', '"+formatdate
+    		                +"', '"+description.get(i+1).text()
+    		                +"', '"+tcNewsID.get(i).text()
+    		                +"','"+count+"');";
+    				//System.out.println(sql);
+    				st.executeUpdate(sql);
+    		}
+            conn.close(); 
+        } catch (Exception e) { 
+            System.err.println("Got an exception! "); 
+            System.err.println(e.getMessage()); 
+        }
+	}
+	
+	//Parsing絤策-update
+	public static void Parsing_Update(String URL,int count) throws Exception{
+		Document doc = Jsoup.connect(URL).get();
+		Elements title = doc.select("title");
+		Elements pubDate = doc.select("pubDate");
+		Elements description = doc.select("description");
+		Elements tcNewsID = doc.select("tcNewsID");
+		Locale local = new Locale("TRADITIONAL_CHINESE");
+		DateFormat df = new SimpleDateFormat("E, dd MMM yyy HH:mm:ss z",local);
+		DateFormat insertdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
+		try { 
+            String url = "jdbc:sqlserver://114.34.8.237;databaseName=Taipower";
+            Connection conn = DriverManager.getConnection(url,"show0308","show0308"); 
+            Statement st = conn.createStatement(); 
+            for(int i=0 ; i<pubDate.size();i++) {
+    			Date date = df.parse(pubDate.get(i).text());
+    			
+    			
+    				String formatdate = insertdf.format(date);
+    				
+    				String sql = "UPDATE dbo.NEWS "
+    						+ "SET Title = '" + title.get(i+1).text()
+    						+ "', Pubtime ='"+formatdate
+    						+"',Description='"+description.get(i+1).text()
+    						+"'WHERE tcNewsID = "+tcNewsID.get(i).text()+";";
+    				System.out.println(sql);
+    				st.executeUpdate(sql);
+
+    		}
+            conn.close(); 
+        } catch (Exception e) { 
+            System.err.println("Got an exception! "); 
+            System.err.println(e.getMessage()); 
+        }
+	}
+	
+	//Parsing絤策-Delete
+	public static void Parsing_Delete(String ID) throws Exception{
+		
+		try { 
+            String url = "jdbc:sqlserver://114.34.8.237;databaseName=Taipower";
+            Connection conn = DriverManager.getConnection(url,"show0308","show0308"); 
+            Statement st = conn.createStatement(); 
+
+    		String sql ="DELETE FROM dbo.NEWS\n"
+    				+"WHERE tcNewsID = "+ ID +";";
+    		System.out.println(sql);
+    		st.executeUpdate(sql);
+
+    		
+            conn.close(); 
+        } catch (Exception e) { 
+            System.err.println("Got an exception! "); 
+            System.err.println(e.getMessage()); 
+        }
+	}
+	
+	//Parsing絤策-Select
+public static void Parsing_Select(String ID) throws Exception{
+		
+		try { 
+            String url = "jdbc:sqlserver://114.34.8.237;databaseName=Taipower";
+            Connection conn = DriverManager.getConnection(url,"show0308","show0308"); 
+            Statement st = conn.createStatement(); 
+
+    		String sql ="SELECT *\n"
+    				+ "FROM dbo.NEWS\n"
+    				+ "WHERE tcNewsID= "+ ID +";";
+    		System.out.println(sql);
+    		st.executeUpdate(sql);
+
+    		
+            conn.close(); 
+        } catch (Exception e) { 
+            System.err.println("Got an exception! "); 
+            System.err.println(e.getMessage()); 
+        }
+	}
+	
+	
 }
